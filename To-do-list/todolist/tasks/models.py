f44701d0ce9d2c15fab0e7fda4c6f5aa252c4d69
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-11-20 10:35:15 +0200

    Docs: initial commint of django files

--- To-do-list/todolist/tasks/models.py
+++ To-do-list/todolist/tasks/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class Task(models.Model):
+    tasks = models.CharField(max_length=200)
+    description = models.TextField()
+    status = models.BooleanField(default='Pending')
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.tasks
--- To-do-list/todolist/todolist/settings.py
+++ To-do-list/todolist/todolist/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-x9vco**3arklkz+odtf949s797ou_4)ql@)uz@coahj+tom&k#'
+SECRET_KEY = 'django-insecure-f9wyk#7z$^o)dz8$j0t2!zo*ky%w)iz!_m_g-9pnugtn6f=hkh'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'tasks',
 ]
 
 MIDDLEWARE = [
