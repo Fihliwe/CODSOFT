f2bec9fc925db8820aa770b6020e492ac4462b85
Fihliwe <136016375+Fihliwe@users.noreply.github.com>
2024-11-26 10:06:34 +0200

    implemented the contact model

--- contact_book/contactbook/contacts/models.py
+++ contact_book/contactbook/contacts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Contact(models.Model):
+    first_name = models.CharField(max_length=50)
+    last_name = models.CharField(max_length=50)
+    phone_number = models.CharField(max_length=15, blank=True, null=True)
+    email = models.EmailField(blank=True, null=True)
+    address = models.TextField(blank=True, null=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
